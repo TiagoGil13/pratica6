a5f96bcf301079a24d298d27ce5e6c8cf0e1037c
TiagoGil13 <tiagogilgm@gmail.com>
2025-04-25 21:24:46 -0300

    pratica6

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    printf("Escreva sua idade: ");
+    int idade;
+    scanf("%d", &idade);
+
+    if (idade >=18) {
+        printf("Você é maior de idade\n");
+    } else {
+        printf ("Você é menor de idade\n");
+    }
+
+}
